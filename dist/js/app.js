3d4f8c81a29cf3ff43cba2dd4110cec9d3c2fee0
chrisharley81 <chrisharley81@gmail.com>
2019-12-03 20:52:06 -0500

    initial styling for nav and showcase / slider functionality

---
+++ dist/js/app.js
@@ -0,0 +1,23 @@
+let i = 0;
+const images = [];
+const time = 3000;
+
+// images
+images[0] = 'images/img1.jpg';
+images[1] = 'images/img2.jpg';
+images[2] = 'images/img3.jpg';
+images[3] = 'images/img4.jpg';
+
+function changeImg() {
+  document.slide.src = images[i];
+
+  if (i < images.length - 1) {
+    i++;
+  } else {
+    let i = 0;
+  }
+
+  setTimeout(changeImg, time);
+}
+
+window.onload = changeImg;
